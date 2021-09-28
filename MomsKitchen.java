91392659e5788f44850ebc1a0618f5daa2ecae81
Shrimad-Bhagwat <sybbhagwat010102@gmail.com>
2021-09-28 18:41:47 +0530

    Working Project 1

--- MomsKitchen.java
+++ MomsKitchen.java
@@ -1,66 +1,106 @@
 import java.util.*;
-class Dish{
+
+class Dish {
     int dishCount;
     Scanner sc = new Scanner(System.in);
-    String[] dishes = new String[10];
-    void setNumber(int dishC){
+    String[] dishes = new String[5];
+
+    void setNumber(int dishC) {
         dishCount = dishC;
     }
-    
-    void setDish(){
+
+    String[] setDish() {
         String[] dishes = new String[dishCount];
-        
+
         for (int i = 0; i < dishes.length; i++) {
-            System.out.print("Enter dish "+i +" name : ");
+            System.out.print("Enter dish " + i + " name : ");
             String value = sc.next();
             dishes[i] = value;
         }
+        return dishes;
     }
-    void printDish(){
-        for (int i = 0; i < dishes.length; i++) {
-            System.out.println("Dish "+i +" name : "+ dishes[i]);
+
+
+}
+
+
+// Main Function
+public class MomsKitchen {
+    public static void printList(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
-    
-}
-public class MomsKitchen {
+    public static int getIndexOfLargest( int[] array ){
+        if ( array == null || array.length == 0 ) return -1; 
+        int largest = 0;
+        for ( int i = 1; i < array.length; i++ ){
+            if ( array[i] > array[largest] ) largest = i;
+        }
+        return largest; 
+    }
+
     public static void main(String[] args) {
-        int dishC=0;
-        System.out.println("Welcome to Mom's Kitchen");
+
+        int dishC = 0,res=0;
+        String[] setDishes = new String[dishC];
+        System.out.println("\n----- Welcome to Mom's Kitchen -----");
         Scanner sc = new Scanner(System.in);
         Dish d = new Dish();
-        System.out.println("Chose option : 1.Show Dishes 2.Set Dishes");
-        int option = sc.nextInt();
-        if(option==1){
-            d.printDish();
-        }
-        else if(option==2){
-            System.out.println("Enter number of dishes: " );
-            dishC = sc.nextInt();
-            d.setNumber(dishC);
-            d.setDish();
-        }
-        else{
-            System.out.println("Wrong Option");
-        }
+        while(true){
 
+            System.out.println("\n----------\nChose User : \n1.Mom \n2.Family Members\n3.Exit");
+            int option = sc.nextInt();
+            if (option == 1) {
+                
+                System.out.println("\n1.Set Dishes \n2.See Choices");
+                int func = sc.nextInt();
+                if(func==1){
 
-        System.out.print("\nHow many members ? ");
-        int memberCount = sc.nextInt();
-        int[] member = new int[memberCount];
-        int[] choice = new int[dishC];
-        System.out.println("Enter dish number to chose");
-        for (int i = 0; i < member.length; i++) {
-            System.out.print("Choice of member "+ i + " ");
-            int index = sc.nextInt();
-            int count = choice[index];
-            choice[index] = ++count;
+                    System.out.println("Enter number of dishes: ");
+                    dishC = sc.nextInt();
+                    d.setNumber(dishC);
+                    setDishes = d.setDish();
+                    continue;
+                }
+                else if(func==2){
+                    System.out.println("Choices Displayed");
+                }
+            } else if (option == 2) {
+               
+                System.out.print("\nHow many members ? ");
+                int memberCount = sc.nextInt();
+                int[] member = new int[memberCount];
+                int[] choice = new int[dishC];
+                System.out.println("Enter dish number to chose");
+                for (int i = 0; i < member.length; i++) {
+                    System.out.print("Choice of member " + i + " ");
+                    int index = sc.nextInt();
+                    int count = choice[index];
+                    choice[index] = ++count;
+                }
+                System.out.println("\nChoices list output");
+                int finalDishIndex = Integer.MIN_VALUE;
+                for (int i = 0; i < choice.length; i++) {
+                    System.out.println("Dish "+setDishes[i]+": "+choice[i]);
+                    if (choice[i] > finalDishIndex) {
+                        finalDishIndex = choice[i];
+                    }
+                }
+                res = finalDishIndex = getIndexOfLargest(choice);
+            }
+            else {
+                System.out.println("Exiting...");
+                break;
+            }
+            try {
+                System.out.println("\n----------\nDish " + setDishes[res] + " will be made\n----------\n");
+                
+            } catch (Exception e) {
+                System.out.println("Some Error Occured!");
+            }
+            
         }
-        System.out.println("\nChoices list output");
-        for (int i = 0; i < choice.length; i++) {
-            System.out.println(choice[i]);
-        }
-
 
     }
 }
